e2f1f86ebfecd3a157d720b253d2ac8eac4cd861
SushanGhimire <sushangmi55@gmail.com>
2020-07-21 22:22:10 +0545

    final blog template

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        lightgray: {
+          100: "#D3D1E0",
+          200: "#A68EA0",
+          300: "#83859B",
+        },
+        charcol: "#34384D",
+        charcolBrown: {
+          100: "#8A534C",
+          200: "#B77C5C",
+        },
+      },
+      fontFamily: {
+        body: ["Oswald"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
